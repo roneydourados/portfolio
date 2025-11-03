ffef62c1c27dbcdf1868c013345e253d00bf50fe
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-11-03 10:41:57 -0400

    portifolio

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-const { data: page } = await useAsyncData('index', () => {
-  return queryCollection('index').first()
-})
+const { data: page } = await useAsyncData("index", () => {
+  return queryCollection("index").first();
+});
 if (!page.value) {
   throw createError({
     statusCode: 404,
-    statusMessage: 'Page not found',
-    fatal: true
-  })
+    statusMessage: "Page not found",
+    fatal: true,
+  });
 }
 
 useSeoMeta({
   title: page.value?.seo.title || page.value?.title,
   ogTitle: page.value?.seo.title || page.value?.title,
   description: page.value?.seo.description || page.value?.description,
-  ogDescription: page.value?.seo.description || page.value?.description
-})
+  ogDescription: page.value?.seo.description || page.value?.description,
+});
 </script>
 
 <template>
@@ -23,13 +23,13 @@ useSeoMeta({
     <LandingHero :page />
     <UPageSection
       :ui="{
-        container: '!pt-0 lg:grid lg:grid-cols-2 lg:gap-8'
+        container: '!pt-0 lg:grid lg:grid-cols-2 lg:gap-8',
       }"
     >
       <LandingAbout :page />
       <LandingWorkExperience :page />
     </UPageSection>
-    <LandingBlog :page />
+    <!-- <LandingBlog :page /> -->
     <LandingTestimonials :page />
     <LandingFAQ :page />
   </UPage>
